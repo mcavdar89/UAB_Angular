c20421beb8b521cb811beed13980772684c312e3
mcavdar89 <mcavdar89@gmail.com>
2026-02-15 14:05:02 +0300

    .

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 import { ResponseModel } from '../../shared/models/response.model';
 import { LoginModel } from '../models/login.model';
 
@@ -8,14 +8,37 @@ import { LoginModel } from '../models/login.model';
   providedIn: 'root',
 })
 export class AuthService {
+  private readonly tokenKey = 'token';
+
+  private _token = signal<string | null>(
+    localStorage.getItem(this.tokenKey)
+  );
+
+  token = computed(() => this._token());
+  isAuthenticated = computed(() => !!this.token());
+
+
 
   http = inject(HttpClient);
 
   signIn(loginModel: LoginModel): Observable<ResponseModel<string>> {
 
-    return this.http.post<ResponseModel<string>>('http://localhost:5100/api/Auth/login', loginModel);
+    return this.http.post<ResponseModel<string>>('http://localhost:5100/api/Auth/login', loginModel).pipe(
+      // map((response) => response),
+      tap((response) => {
+        if (response.isSuccess && response.data) {
+          //localStorage.setItem('token', response.data);
+          this.setToken(response.data);
+        }
+      })
+    );
 
   }
+
+
+  private setToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  };
 
 
 
