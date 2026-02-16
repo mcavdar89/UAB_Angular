85491fce0c1acd169ca8795b74a364d835d615d9
mcavdar89 <mcavdar89@gmail.com>
2026-02-16 14:44:57 +0300

    .

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, effect, inject, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from './features/auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,29 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('UAB.AracKiralama.WEb.UI');
+
+  authService = inject(AuthService);
+
+  isLogin = computed(() => this.authService.isAuthenticated());
+
+
+  route = inject(Router);
+
+
+  /**
+   *
+   */
+  constructor() {
+    effect(() => {
+
+      if (!this.isLogin()) {
+        this.route.navigate(['/auth', 'login']);
+
+      }
+
+    });
+
+  }
+
+
 }
